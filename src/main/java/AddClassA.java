7557ee08332f2aa801595462bf2136501fef5147
wulibaye <475029936@qq.com>
2018-11-13 01:17:04 +0800

    全部提交

--- src/main/java/AddClassA.java
+++ src/main/java/AddClassA.java
@@ -1,6 +1,5 @@
 public class AddClassA {
     public static void main(String[] args) {
         System.out.println("增加一个类，测试一下git的用法");
-        修改文件
     }
 }
---
+++ src/main/java/AddClassB.java
@@ -0,0 +1,2 @@
+public class AddClassB {
+}
